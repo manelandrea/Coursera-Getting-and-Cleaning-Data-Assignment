399c18337fa2fa2a769ade7456e222579b970690
manelandrea <nel_cementina@yahoo.com>
2018-03-09 12:18:44 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,10 @@
 
-library(plyr)
+library(reshape2)
 
+#load features table
+featureNames <- read.table("features.txt")
+#load activity labels
+activityLabels <- read.table("activity_labels.txt", header = FALSE)
 # Read training data
 subject_train <-  read.table("train/subject_train.txt", header = FALSE)
 y_train <-  read.table("train/y_train.txt", header = FALSE)
@@ -27,29 +31,28 @@ colnames(subject_merge) <- "Subject"
 completeData <- cbind(x_merge,y_merge,subject_merge)
 
 #TO DO No. 2: Extract only the measurements on the mean and standard deviation for each measurement.
-meanSTD <- grep(".*Mean.*|.*Std.*", names(completeData), ignore.case=TRUE)
-cols <- c(meanSTD, 562, 563)
-dim(completeData)
-
-newData <- completeData[,cols]
-dim(newData)
+meanSTD <- grep(".*Mean.*|.*Std.*", featureNames[,2], ignore.case=TRUE)
+meanSTD.names <-featureNames[meanSTD,2]
 
 #TO DO NO. 3: Use descriptive activity names to name the activities in the data set
 # update values with correct activity names
-#load features table
-featureNames <- read.table("features.txt")
-#load activity labels
-activityLabels <- read.table("activity_labels.txt", header = FALSE)
-newData[,1] <- as.character(newData[,1])
-for (i in 1:6){
-  newData[,1][newData[,1] == i] <- as.character(activityLabels[i,2])
-}
+meanSTD.names = gsub('-mean', 'Mean', meanSTD.names)
+meanSTD.names = gsub('-std', 'Std', meanSTD.names)
+meanSTD.names <- gsub('[-()]', '', meanSTD.names)
+
+train <- x_train[meanSTD]
+train <- cbind(subject_train, y_train , train)
+test <- x_test[meanSTD]
+test <-  cbind(subject_test , y_test , test)
+# merge datasets and add labels
+newData <-rbind(train, test)
+colnames(newData) <- c("subject", "activity", meanSTD.names)
 
 newData[,1] <- as.factor(newData[,1])
 #TO DO NO. 4: Appropriately label the data set with descriptive variable names.
 names(newData)<-gsub("Acc", "Accelerometer", names(newData))
 names(newData)<-gsub("Gyro", "Gyroscope", names(newData))
-names(extractedData)<-gsub("BodyBody", "Body", names(newData))
+names(newData)<-gsub("BodyBody", "Body", names(newData))
 names(newData)<-gsub("Mag", "Magnitude", names(newData))
 names(newData)<-gsub("^t", "Time", names(newData))
 names(newData)<-gsub("^f", "Frequency", names(newData))
@@ -62,8 +65,11 @@ names(newData)<-gsub("gravity", "Gravity", names(newData))
 
 #TO NO. 5: From the data set in step 4, creates a second, independent tidy data set with the 
 #average of each variable for each activity and each subject.
-newData$Subject <- as.factor(newData$Subject)
-newData$Activity <- as.factor(newData$Activity)
-tidyData <- aggregate(. ~Subject + Activity, newData, mean)
-tidyData <- tidyData[order(tidyData$Subject,tidyData$Activity),]
-write.table(tidyData, file = "TidyData.txt", row.names = FALSE)
+# turn activities & subjects into factors
+newData$activity <-  factor(newData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+newData$subject  <- as.factor(newData$subject)
+
+newData.melted <- melt(newData, id = c("subject", "activity"))
+newData.mean <- dcast(newData.melted, subject + activity ~ variable, mean)
+
+write.table(newData.mean, "tidyData.txt", row.names = FALSE, quote = FALSE)
